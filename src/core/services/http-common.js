f1dce9823925cb20222366cfc7c9c22eb3547bcb
2CTO <u201521691@upc.edu.pe>
2022-09-19 12:03:33 -0500

    feat: deployed app

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://app-erant.herokuapp.com",
+  baseURL: "https://erentcarbackend.herokuapp.com/api/v1",
   headers: {
     "Content-type": "application/json"
   },
